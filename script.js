783960e81ada67aac4d64a0e4fe385466434427a
Eliza X. <rysaeva96@mail.ru>
2026-02-12 20:35:15 +0300

    выполнено задание 12

--- script.js
+++ script.js
@@ -31,9 +31,11 @@ const appData = {
     servicesNumber: {},
     init: function () {
         appData.addTitle()
-
+        appData.getSlider();
         startBtn.addEventListener('click', appData.start)
         buttonPlus.addEventListener('click', appData.addScreenBlock)
+        appData.attachEventListeners();
+        appData.checkFields();
     },
     addTitle: function () {
         document.title = title.textContent;
@@ -42,14 +44,15 @@ const appData = {
         appData.addScreens()
         appData.addServices()
         appData.addPrices();
-        // appData.getServicePercentPrices();
         // appData.logger();
         appData.showResult();
     },
-    showResult: function() {
-        total.value = appData.screenPrice;
+    showResult: function () {
+        total.value = appData.fullPrice;
         totalCountOther.value = appData.servicePricesPercent + appData.servicePricesNumber
         fullTotalCount.value = this.fullPrice
+        totalCountRollback.value = appData.totalCountWithRollback
+        totalCount.value = appData.totalScreenCount
     },
     addScreens: function () {
         let screens = document.querySelectorAll('.screen')
@@ -58,62 +61,100 @@ const appData = {
             const select = screen.querySelector('select')
             const input = screen.querySelector('input')
             const selectName = select.options[select.selectedIndex].textContent
+            const count = +input.value
             appData.screens.push({
                 id: index,
                 name: selectName,
-                price: +select.value * +input.value
+                price: +select.value * count,
+                count: count
             })
         })
-        console.log(appData.screens);
     },
-    addServices: function() {
-        otherItemsPercent.forEach(function(item) {
+    addServices: function () {
+        otherItemsPercent.forEach(function (item) {
             const check = item.querySelector('input[type=checkbox')
             const label = item.querySelector('label')
             const input = item.querySelector('input[type=text]')
-            if(check.checked) {
+            if (check.checked) {
                 appData.servicesPercent[label.textContent] = +input.value
             }
         })
-        otherItemsNumber.forEach(function(item) {
+        otherItemsNumber.forEach(function (item) {
             const check = item.querySelector('input[type=checkbox')
             const label = item.querySelector('label')
             const input = item.querySelector('input[type=text]')
-            if(check.checked) {
+            if (check.checked) {
                 appData.servicesNumber[label.textContent] = +input.value
             }
-            
         })
     },
     addScreenBlock: function () {
         const cloneScreen = screens[0].cloneNode(true)
         screens[screens.length - 1].after(cloneScreen)
+        screens = document.querySelectorAll('.screen')
+        appData.attachEventListeners();
+        appData.checkFields();
+    },
+    attachEventListeners: function () {
+        screens.forEach(screen => {
+            const select = screen.querySelector('select')
+            const input = screen.querySelector('input')
+
+            select.removeEventListener('change', appData.checkFields)
+            input.removeEventListener('input', appData.checkFields)
+
+            select.addEventListener('change', appData.checkFields)
+            input.addEventListener('input', appData.checkFields)
+        })
+    },
+    checkFields: function () {
+        let allFilled = true
+        const mainControlSelect = document.querySelectorAll('.main-controls__item.screen')
+        mainControlSelect.forEach(block => {
+            const select = block.querySelector('select')
+            const input = block.querySelector('input')
+            const selectValid = select.value !== ''
+            const inputValid = input.value.trim() !== '' && parseInt(input.value) > 0;
+            if (!selectValid || !inputValid) {
+                allFilled = false;
+            }
+        })
+        startBtn.disabled = !allFilled
     },
     addPrices: function () {
+        let totalScreenCount = 0
+        appData.screens.forEach(function(screen) {
+            totalScreenCount += screen.count
+        })
+        appData.totalScreenCount = totalScreenCount
+
+        appData.screenPrice = 0
         for (let screen of appData.screens) {
             appData.screenPrice = appData.screens.reduce((sum, screen) => sum + (+screen.price), 0)
         }
         for (let key in appData.servicesNumber) {
-                appData.servicePricesNumber += appData.servicesNumber[key]
+            appData.servicePricesNumber += appData.servicesNumber[key]
         }
         for (let key in appData.servicesPercent) {
-                appData.servicePricesPercent += appData.screenPrice * (appData.servicesPercent[key] / 100)
+            appData.servicePricesPercent += appData.screenPrice * (appData.servicesPercent[key] / 100)
         }
         appData.fullPrice = appData.screenPrice + appData.servicePricesPercent + appData.servicePricesNumber
+        const rollbackValue = +appData.getRollback();
+        appData.totalCountWithRollback = appData.fullPrice - (appData.fullPrice * (rollbackValue / 100))
     },
-    getServicePercentPrices: function () {
-        appData.servicePercentPrice = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100))
+    getSlider: function () {
+        const mainControlsRollback = document.querySelector('.main-controls__item.rollback')
+        const span = mainControlsRollback.querySelector('span')
+        inputRange.addEventListener('input', () => {
+            const value = inputRange.value
+            appData.rollback = value
+            span.textContent = value + '%';
+            appData.addPrices();
+            appData.showResult();
+        })
     },
-    getRollbackMessage: function (price) {
-        if (price >= 30000) {
-            return "Даем скидку в 10%"
-        } else if (price >= 15000 && price < 30000) {
-            return "Даем скидку в 5%"
-        } else if (price < 15000 && price > 0) {
-            return "Скидка не предусмотрена"
-        } else if (price <= 0) {
-            return "Что-то пошло не так"
-        }
+    getRollback: function () {
+        return +appData.rollback;
     },
     logger: function () {
         console.log(this.fullPrice);
